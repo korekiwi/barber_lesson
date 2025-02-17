ecd3c3e110fce55c7be2619d2ebf5f510e095b95
Korekiwi <foxiknyan@gmail.com>
2025-02-17 21:10:41 +0300

    формы

--- barber/urls.py
+++ barber/urls.py
@@ -2,10 +2,13 @@ from django.contrib import admin
 from django.urls import path
 from django.conf import settings
 from django.conf.urls.static import static
+from core import views
+
 
 urlpatterns = (
     [
         path("admin/", admin.site.urls),
+        path("thanks/", views.ThanksView.as_view(), name="thanks"),
     ]
     + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
     + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
--- core/views.py
+++ core/views.py
@@ -8,3 +8,27 @@ MENU = [
     # {'title': 'Оставить отзыв', 'url': '/review/create/', 'active': True},
     {'title': 'Запись на стрижку', 'url': '#orderForm', 'active': True},
 ]
+
+# Импорт базовой вьюхи
+from django.views.generic import View, TemplateView
+
+
+# # Создание класса-наследника View
+# class ThanksView(View):
+#     # Переопределение метода get
+#     def get(self, request):
+#         # Возврат результата
+
+#         context = {
+#             'menu': MENU
+#         }
+#         return render(request, 'thanks.html', context)
+
+
+class ThanksView(TemplateView):
+    template_name = 'thanks.html'
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context['menu'] = MENU
+        return context
